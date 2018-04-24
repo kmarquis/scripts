b2101873d9ed1d0e29c2204f587d2dd658f33b2e
Keeran Marquis <k.marquis@kd-tech.co.uk>
2018-04-24 23:50:23 +0100

    Update to modules, added/updated new scipts

--- pyez/showRoute.py
+++ pyez/showRoute.py
@@ -2,10 +2,13 @@
 import sys
 import os
 import getpass
+import json
+import xmltodict
+import platform
+from lxml import etree
+from huepy import *
 from jnpr.junos import Device
-from jnpr.junos.op.routes import RouteTable
 from jnpr.junos.exception import *
-
 
 def deviceLogins():
     host = sys.argv[1]
@@ -20,23 +23,34 @@ def getRoutes(dev):
     try:
         dev.open()
     except ConnectError as conErr:
-        print("Cannot connect to device: {0}".format(conErr))
+        print(bad("Cannot connect to device: {0}".format(conErr)))
         sys.exit(1)
     except ConnectAuthError as conAuth:
-        print("Cannot connect to device: {0}".format(conAuth))
+        print(bad("Cannot connect to device: {0}".format(conAuth)))
         sys.exit(1)
     except ConnectTimeoutError as conTimeOut:
-        print("Cannot connect to device: {0}".format(conAuth))
+        print(bad("Cannot connect to device: {0}".format(conAuth)))
         sys.exit(1)
     hostname = dev.facts['hostname']
-    routetbl = RouteTable(dev).get(showRoute)
-    for prefix, details in routetbl.items():
-        pfixDetails = dict(details)
-        pfix = prefix
-    print('Route: ' + pfix)
-    print('Protocol: '+ pfixDetails['protocol'])
-    print('Inteface: ' + pfixDetails['via'] )
-    print('BGP neighbor: ' + pfixDetails['learnt'])
+    routetbl = dev.rpc.get_route_information(table='inet.0', destination=showRoute)
+    routeJson = json.loads(json.dumps(xmltodict.parse(etree.tostring(routetbl))))
+    for route, routeVal in routeJson["route-information"].items():
+        routeValDict = dict(routeVal)
+        if routeValDict["rt"]["rt-entry"]["active-tag"] == '*':
+            route = routeValDict["rt"]["rt-destination"]
+            protocol = routeValDict["rt"]["rt-entry"]["protocol-name"]
+            next_hop = routeValDict["rt"]["rt-entry"]["nh"]["to"]
+            if protocol == 'BGP':
+                neighbor = routeValDict["rt"]["rt-entry"]["learned-from"]
+                print(good(showRoute + " route is " + route))
+                print(good("The BGP next-hop neighbor is " + neighbor))
+                if platform.system() == 'windows':
+                    os.system('nslookup ' + neighbor)
+                else:
+                    os.system('host ' + neighbor)
+            else:
+                print(good(showRoute + " route is " + route))
+                print(good(showRoute + " is learnt by " + protocol + " with the nexthop " + next_hop))
     dev.close()
 
 def main():
@@ -44,6 +58,6 @@ def main():
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Please provide the following "python3 showRoute.py host route"')
+        print(info(lightred('Please provide the following "python3 showRoute.py host route"')))
         sys.exit(1)
     main()
